55666ecf7b5a49985678074d9293ba2545a32b6b
Krunal Patel <patelkrunal821@gmail.com>
2018-08-13 18:24:57 +0530

    add password comparison in sign up form just.

--- app/src/main/java/krunal/com/example/loginapp/SignUpActivity.java
+++ app/src/main/java/krunal/com/example/loginapp/SignUpActivity.java
@@ -65,9 +65,7 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
-
     private void SetupSpinner() {
-
         ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.array_gender_options, android.R.layout.simple_spinner_item);
 
@@ -146,7 +144,7 @@ public class SignUpActivity extends AppCompatActivity {
         String re_passwordInput = mRe_Password.getText().toString().trim();
 
         if (re_passwordInput.isEmpty()) {
-            mRe_Password.setError("Field can't be empty");
+            mRe_Password.setError("Field can't be empty@!");
             return false;
         }else if (re_passwordInput.length() < 4 || re_passwordInput.length() > 10){
             mRe_Password.setError("between 4 and 10 alphanumeric characters");
